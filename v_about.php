3520f0533d5c5d98371167b3702a1f13b03fd117
WP2-SaifAmannuloh <102023761+WP2-SaifAmannuloh@users.noreply.github.com>
2022-04-15 20:18:00 +0700

    Update v_about.php

--- v_about.php
+++ v_about.php
@@ -2,26 +2,25 @@
     <h1><?php echo $judul ?></h1>
     <h4>Nama</h4>
     <ul type="disc">
-        <li>Nama Depan : Farrel</li>
-        <li>Nama Belakang : Abdullah</li>
-        <li>NIM : 17190310</li>
+        <li>Nama Depan : Saif </li>
+        <li>Nama Belakang : Amannuloh</li>
+        <li>NIM : 17190375</li>
     </ul>
 
     <h4> Alamat </h4>
     <ul type="none">
-        <li>Perumahan Bumi Indah Pesona RT01/013</li> 
-        <li>Kel. Kalisuren, Kec. Tajurhalang, Kab. Bogor, JawaBarat</li>
+        <li> Gg.Manggis Rt006/Rw 001 </li> 
+        <li>Kel.Pasir- Putih, Kec. Sawangan, Kab.JawaBarat</li>
     </ul>
 
     <h4> Tempat Lahir </h4>
     <ul type="none">
-        <li>Jakarta</li>
+        <li>Purbalingga</li>
     </ul>
 
     <h4>Hobi</h4>
     <ul type="square">
-        <li>Bermain Basket</li>
-        <li>Bermain Game</li>
-        <li>Mendengarkan Musik</li>
+        <li>Bermain Bola</li>
+   
     </ul>
 </section>
